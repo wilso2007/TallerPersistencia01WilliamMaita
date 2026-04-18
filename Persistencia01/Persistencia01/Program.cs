0329a58a36baf51a1b5340243b3327af241c6963
wilso2007 <williamdavidmaita@gmail.com>
2026-04-18 18:09:06 -0400

    DIRECTORIO

--- Persistencia01/Persistencia01/Program.cs
+++ Persistencia01/Persistencia01/Program.cs
@@ -1,12 +1,6 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: Usuario
- * Fecha: 18/4/2026
- * Hora: 5:49 p. m.
- * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
- */
+﻿
 using System;
+using System.IO;
 
 namespace Persistencia01
 {
@@ -16,7 +10,18 @@ namespace Persistencia01
 		{
 			Console.WriteLine("TALLER SECCION B");
 			
+			//Creamos un directorio
 			
+				string rutaRaiz = Path.Combine(AppDomain .CurrentDomain .BaseDirectory ,"DATOS IUJO");
+				string rutaReportes = Path.Combine (rutaRaiz,"reportes");
+				
+				Console.WriteLine (rutaRaiz);
+				Console .WriteLine (rutaReportes);
+				
+				if(Directory .Exists (rutaReportes )){
+					Directory.CreateDirectory (rutaReportes );
+					Console .WriteLine ("directorio creado correctamente");
+				}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
