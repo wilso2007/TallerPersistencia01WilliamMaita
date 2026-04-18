6ac2278ad07ccdda05fa787100b821c9ce416c8a
wilso2007 <williamdavidmaita@gmail.com>
2026-04-18 17:59:09 -0400

    Taller de persistencia seccion B

---
+++ Persistencia01/Persistencia01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Usuario
+ * Fecha: 18/4/2026
+ * Hora: 5:49 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Persistencia01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("TALLER SECCION B");
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
